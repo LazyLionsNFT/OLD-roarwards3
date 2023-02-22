b09f199f94274665d8281181a94db1b89a8240a9
Luiz Carvalho <luizcarvalhohen@gmail.com>
2023-02-23 00:34:23 +0500

    url token

--- pages/[token].tsx
+++ pages/[token].tsx
@@ -5,15 +5,19 @@ import { useConnectWallet } from '@web3-onboard/react'
 
 const Token = () => {
     const [{ wallet, connecting }, connect, disconnect] = useConnectWallet()
-    const router = useRouter()
-    const token = router.asPath;
-    const address = {address: wallet?.accounts[0]?.address};
+  
 
-    console.log('token');
-    console.log(address);
-    console.log(token.slice(1));
+
     
     useEffect(() => {
+        const router = useRouter()
+        const token = router.asPath;
+        const address = wallet?.accounts[0]?.address;
+
+        console.log('token');
+        console.log(address);
+        console.log(token.slice(1));
+
         axios.post("http://3.144.152.191:3000/connect-wallet", {walletAddress: address},
         {headers: {
             'Authorization': token.slice(1),
